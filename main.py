1a15a0945f9394f7d6b52c0103d8ca733e646a49
ayeshaali02 <ayesha.ali2110@gmail.com>
2022-12-28 15:18:11 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+from hangman_words import words
+from hangman_art import logo
+from hangman_art import stages
+print(logo)
+chosen_word= random.choice(words)
+print(chosen_word)
+display=[]
+underscore="_"
+lives=6
+for letter in chosen_word:
+    underscore="_"
+    display.append(underscore)
+end_of_game= False
+while end_of_game==False:
+    guess= input("Guess a letter: ")
+    if guess in display:
+        print(f"You have already guessed {guess}.")
+    for position in range(0,len(chosen_word)):
+        letter= chosen_word[position]
+        if letter==guess:
+            display[position]=letter
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives-=1
+        if lives==0:
+            end_of_game= True
+            print("You lose")
+    print(stages[lives])
+    print(f"{' '.join(display)}")
+    if underscore not in display:
+        end_of_game= True
+        print("You win!")
+
+
+
+
